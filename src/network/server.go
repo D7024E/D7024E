1281ee5369fef104b50afc417f458b6ea56e6df0
Denne213 <dennist1@live.se>
2022-09-12 10:15:52 +0200

    added address of node

--- src/network/server.go
+++ src/network/server.go
@@ -3,6 +3,7 @@ package network
 import (
 	"D7024E/log"
 	"encoding/json"
+	"net"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -16,7 +17,7 @@ type Object struct {
 var objects []Object
 
 func createObject(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	// w.Header().Set("Content-Type", "application/json")
 	var object Object
 	_ = json.NewDecoder(r.Body).Decode(&object)
 	objects = append(objects, object)
@@ -24,12 +25,12 @@ func createObject(w http.ResponseWriter, r *http.Request) {
 }
 
 func getObjects(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	// w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(objects)
 }
 
 func getObject(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	// w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 
 	for _, item := range objects {
@@ -41,27 +42,43 @@ func getObject(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&Object{})
 }
 
-func loggingMiddleware(next http.Handler) http.Handler {
+func headerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Do stuff here
-		// log.INFO(r.RequestURI)
-		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		w.Header().Set("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
 	})
 }
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.INFO(r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func getAddress() net.Addr {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.FATAL("Failed to retrieve own IP")
+	}
+
+	defer conn.Close()
+	return conn.LocalAddr().(*net.UDPAddr)
+}
+
 func StartRouter() {
-	log.INFO("Initiated Server on ip:TODO and port:TODO")
+	log.INFO("Initiated on %v", getAddress())
 	router := mux.NewRouter()
 
 	objects = append(objects, Object{Name: "Test_Object", Hash: "TEST_HASH"})
 	// objects = append(objects, Object{Name: "THIS_IS_NAME", Hash: "THIS_IS_HASH"})
 
 	router.HandleFunc("/objects", createObject).Methods("POST")
-	router.HandleFunc("/debug/objects", getObjects).Methods("GET")
+	router.HandleFunc("/all/objects", getObjects).Methods("GET")
 	router.HandleFunc("/objects/{hash}", getObject).Methods("GET")
 
+	router.Use(headerMiddleware)
 	router.Use(loggingMiddleware)
 
-	http.ListenAndServe(":8000", router)
+	log.FATAL("Router has stopped working", http.ListenAndServe(":8000", router))
 }
