649f8eb7a0a19ab5af349b078ead217212bd6222
Dennis Trollsfjord <dennist1@live.se>
2022-09-13 16:30:01 +0200

    added var for err

--- src/config/dotenv.go
+++ src/config/dotenv.go
@@ -7,14 +7,17 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var Debug bool // Debug mode
+var (
+	Debug bool  // Debug mode
+	err   error // error given from enviroment
+)
 
 /**
  * Load enviroment variables
  */
 func init() {
-	err := godotenv.Load("../.env")
-
+	_ = godotenv.Load("../.env")
+	// err := nil
 	Debug, err = strconv.ParseBool(os.Getenv("DEBUG"))
 	if err != nil {
 		Debug = false
