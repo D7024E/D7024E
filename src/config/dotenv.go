fa427099f25d988f4c4aad3553ba969e4813e479
naddah-5 <andersoskardahl@hotmail.com>
2022-09-13 16:13:49 +0200

    remote partial fix

--- src/config/dotenv.go
+++ src/config/dotenv.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"log"
 	"os"
 	"strconv"
 
@@ -12,12 +11,9 @@ var Debug bool
 
 func init() {
 	err := godotenv.Load("../.env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	Debug, err = strconv.ParseBool(os.Getenv("DEBUG"))
 	if err != nil {
-		log.Fatal("Error converting to Debug to bool")
+		Debug = false
 	}
 }
