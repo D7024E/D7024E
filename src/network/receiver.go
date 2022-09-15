300e5c6eaa49ce69667b362088bf98dc5b4e17e8
Dennis Trollsfjord <dennist1@live.se>
2022-09-15 14:30:03 +0200

    re:added receiver and comment routes

---
+++ src/network/receiver.go
@@ -0,0 +1,30 @@
+package network
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func Receiver() {
+	// Activate a listener on port 4001.
+	ln, err := net.Listen("tcp", ":4001")
+	fmt.Println("Listening on port: 4001")
+
+	if err != nil {
+		fmt.Println("There was an error:", err)
+	}
+	// Accepts incoming connection.
+	conn, _ := ln.Accept()
+
+	// Infinite loop that listens on port 4001, when it receives a message it prints it out.
+	for {
+		// Innitiates a new buffered reader on port 4000. The port etc is defined upstream through "conn".
+		// Specifically by "ln".
+		fmt.Println("Waiting for message...")
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		// Print out the message that was just received, if any, then continues the loop.
+		fmt.Print("Received a message: ", string(message))
+	}
+
+}
