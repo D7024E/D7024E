747fa957340f32a798d71f87b1e5a91898571473
Anders Dahl <andersoskardahl@hotmail.com>
2022-09-16 11:26:34 +0200

    48 implement json marshalling (#50)
    * base for marshalling
    
    * marshaling and unmarshaling for RPC
    
    Co-authored-by: Dennis Trollsfjord <dennist1@live.se>

---
+++ src/network/rpcmarshal.go
@@ -0,0 +1,40 @@
+package network
+
+import (
+	"encoding/json"
+)
+
+type RPC struct {
+	Cmd     string
+	Contact []string
+	Id      any
+}
+
+// A basic test is bellow, move it to main for testing.
+// Takes a rpc struct and marshalls it to JSON, writes it in res.
+func RpcMarshal(rpc RPC, res *[]byte) {
+	rpcJSON, _ := json.Marshal(rpc)
+	*res = rpcJSON
+}
+
+// Takes a marshaled message and unmarshals it to a RPC struct and writes it to res.
+func RpcUnmarshal(msg []byte, res *RPC) {
+	json.Unmarshal(msg, res)
+}
+
+// var testJSON []byte
+// var testOut network.RPC
+// testid := Contact{
+// 	ID:       node.NewRandomKademliaID(),
+// 	Address:  "THIS IS ADDRESS",
+// 	Distance: node.NewRandomKademliaID(),
+// }
+// test := network.RPC{
+// 	Cmd:     "test",
+// 	Contact: []string{"1", "2", "3"},
+// 	Id:      testid,
+// }
+// network.RpcMarshal(test, &testJSON)
+// fmt.Println(testJSON)
+// network.RpcUnmarshal(testJSON, &testOut)
+// fmt.Println(testOut)
