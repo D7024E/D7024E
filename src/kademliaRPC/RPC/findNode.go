12f7792ff7afa2f1268811ce70acc0776287a297
naddah-5 <andersoskardahl@hotmail.com>
2022-09-17 16:23:21 +0200

    update

--- src/kademliaRPC/RPC/findNode.go
+++ src/kademliaRPC/RPC/findNode.go
@@ -1,1 +1,16 @@
 package rpc
+
+import (
+	"D7024E/node/bucket"
+	"D7024E/node/id"
+	"D7024E/node/kademlia"
+)
+
+func FindNode(destNode id.KademliaID) {
+	instance := kademlia.GetInstance()
+	me := instance.Me.ID
+	rt := bucket.GetInstance()
+
+	contact := rt.FindClosestContacts(&destNode, 1)
+
+}
