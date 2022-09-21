2d69a356d235e66659854537c4f2d08cd2595a31
naddah-5 <andersoskardahl@hotmail.com>
2022-09-21 16:14:13 +0200

    wip

---
+++ src/node/kademlia/algorithms/nodeLookup.go
@@ -0,0 +1,31 @@
+package kademlia
+
+import (
+	"D7024E/config"
+	rpc "D7024E/kademliaRPC/RPC"
+	"D7024E/log"
+	"D7024E/node/bucket"
+	"D7024E/node/contact"
+	"D7024E/node/id"
+)
+
+func NodeLookup(destNode id.KademliaID, batch []contact.Contact) (closest contact.Contact) {
+	rt := bucket.GetInstance()
+	node := rt.Me
+
+	if len(batch) == 0 {
+		batch = rt.FindClosestContacts(&destNode, config.Alpha)
+	}
+	var newBatch [][]contact.Contact
+	// For each of the alpha nodes in "batch", send a findNode RPC and append the result to "newBatch"
+	for i := 0; i < len(batch); i++ {
+		var kN []contact.Contact
+		kN, err := rpc.FindNode(batch[i], destNode)
+		if err != nil {
+			log.ERROR("%v", err)
+		} else {
+			newBatch = append(newBatch, kN)
+		}
+	}
+
+}
