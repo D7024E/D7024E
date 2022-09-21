bccdf5a350f8313948857298c1bbd0617978b85f
Denne213 <dennist1@live.se>
2022-09-21 17:09:48 +0200

    tests for store rpc

--- src/kademliaRPC/RPC/store.go
+++ src/kademliaRPC/RPC/store.go
@@ -5,65 +5,51 @@ import (
 	"D7024E/network/requestHandler"
 	"D7024E/network/sender"
 	"D7024E/node/contact"
-	"D7024E/node/id"
 	"D7024E/node/stored"
-	"net"
 )
 
 // STORE RPC
 // Attempt to store value into target node. If successful return true otherwise
 // return false.
 func StoreRequest(me contact.Contact, target contact.Contact, value stored.Value) bool {
-	// Get pointer to request id instance.
-	requestInstance := requestHandler.GetInstance()
+	reqID := newValidRequestID()
+	message := storeRequestMessage(me, reqID, value)
+	ip := parseIP(target.Address)
+	sender.UDPSender(ip, 4001, message)
+	err := requestHandler.GetInstance().ReadResponse(reqID, &message)
+	return !isError(err)
 
-	// Find valid request id then proceed.
-	var reqID string
-	var err error
-	for {
-		reqID = id.NewRandomKademliaID().String()
-		err = requestInstance.NewRequest(reqID)
-		if err == nil {
-			break
-		}
-	}
+}
 
-	// Marshal message then send it to node.
-	var message *[]byte
+// Creates the rpc message for store request utilizing json marshaling.
+func storeRequestMessage(me contact.Contact, reqID string, value stored.Value) []byte {
+	var message []byte
 	rpcmarshal.RpcMarshal(rpcmarshal.RPC{
 		Cmd:     "STRE",
 		Contact: me,
 		ReqID:   reqID,
 		Content: value,
-	}, message)
-	sender.UDPSender(net.ParseIP(target.Address), 4001, *message)
-
-	// Attempt read the response, if successful return true, otherwise false.
-	err2 := requestInstance.ReadResponse(reqID, message)
-	if err2 != nil {
-		return false
-	} else {
-		return true
-	}
-
+	}, &message)
+	return message
 }
 
 // STORE RPC Response
 // Stores the given value. Then return a rpc message to inform the requesting
 // node that the value is stored.
 func StoreRespond(me contact.Contact, target contact.Contact, reqID string, value stored.Value) {
-	// Create rpc which will be sent.
-	rpcMessage := rpcmarshal.RPC{
+	stored.GetInstance().Store([]stored.Value{value})
+	message := storeRespondMessage(me, reqID)
+	ip := parseIP(target.Address)
+	sender.UDPSender(ip, 4001, message)
+}
+
+// Creates the Store Respond by json marshaling of rpc.
+func storeRespondMessage(me contact.Contact, reqID string) []byte {
+	var message []byte
+	rpcmarshal.RpcMarshal(rpcmarshal.RPC{
 		Cmd:     "ERTS",
 		Contact: me,
 		ReqID:   reqID,
-	}
-
-	// Store the value within node.
-	stored.GetInstance().Store([]stored.Value{value})
-
-	// Marshal rpc and send it to target.
-	var message *[]byte
-	rpcmarshal.RpcMarshal(rpcMessage, message)
-	sender.UDPSender(net.ParseIP(target.Address), 4001, *message)
+	}, &message)
+	return message
 }
