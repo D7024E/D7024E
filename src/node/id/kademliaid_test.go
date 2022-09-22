6a284363f2bfa551a383175a5bc6ff45c8dbde62
erik99s <erisre-8@student.ltu.se>
2022-09-22 10:38:24 +0200

    100% on kademliaidtest

--- src/node/id/kademliaid_test.go
+++ src/node/id/kademliaid_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestCalcDistanceSucces(t *testing.T) {
+func TestCalcDistanceEqualsSucces(t *testing.T) {
 	idMe := NewRandomKademliaID()
 	idTarget := NewRandomKademliaID()
 
@@ -20,7 +20,7 @@ func TestCalcDistanceSucces(t *testing.T) {
 	}
 }
 
-func TestCalcDistanceFail(t *testing.T) {
+func TestCalcDistanceEqualsFail(t *testing.T) {
 	idMe := NewRandomKademliaID()
 	idTarget := NewRandomKademliaID()
 
@@ -34,6 +34,49 @@ func TestCalcDistanceFail(t *testing.T) {
 	}
 
 	if result1.Equals(&result2) {
+		t.FailNow()
+	}
+}
+
+func TestCalcDistanceLessSucces(t *testing.T) {
+	idMe := NewRandomKademliaID()
+	idTarget := NewRandomKademliaID()
+
+	result1 := idMe.CalcDistance(idTarget)
+
+	result2 := KademliaID{}
+	for i := 0; i < IDLength; i++ {
+		result2[i] = idMe[i] ^ idTarget[i]
+	}
+
+	if result1.Less(&result2) {
+		t.FailNow()
+	}
+}
+
+func TestCalcDistanceLessFail(t *testing.T) {
+	idMe := NewRandomKademliaID()
+	idTarget := NewRandomKademliaID()
+
+	idTarget2 := NewRandomKademliaID()
+
+	result1 := idMe.CalcDistance(idTarget)
+
+	result2 := KademliaID{}
+	for i := 0; i < IDLength; i++ {
+		result2[i] = idMe[i] ^ idTarget2[i]
+	}
+
+	if !result1.Less(&result2) {
+		t.FailNow()
+	}
+}
+
+func TestNewKademliaIDSuccess(t *testing.T) {
+	id := NewRandomKademliaID()
+	idString := id.String()
+	id2 := NewKademliaID(idString)
+	if !id.Equals(id2) {
 		t.FailNow()
 	}
 }
