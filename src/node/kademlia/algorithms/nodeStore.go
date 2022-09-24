94e300a970144bce5d417a21d75680d9ed5726fb
Dennis Trollsfjord <dennist1@live.se>
2022-09-24 09:58:24 +0200

    added nodeStore and nodeValueLookup

--- src/node/kademlia/algorithms/nodeStore.go
+++ src/node/kademlia/algorithms/nodeStore.go
@@ -3,33 +3,30 @@ package algorithms
 import (
 	rpc "D7024E/kademliaRPC/RPC"
 	"D7024E/node/contact"
-	"D7024E/node/id"
 	"D7024E/node/kademlia"
 	"D7024E/node/stored"
-	"errors"
 	"sync"
 )
 
-func NodeValueLookup(valueID id.KademliaID) (stored.Value, error) {
+func NodeStore(value stored.Value) bool {
 	closest := []contact.Contact{{Address: "172.21.0.2"}, {Address: "172.21.0.3"}, {Address: "172.21.0.4"}} // TODO NodeLookup(valueID)
 	var wg sync.WaitGroup
-	var result []stored.Value
+	completed := true
 	for _, c := range closest {
 		wg.Add(1)
 		target := c
 		go func() {
 			defer wg.Done()
-			val, err := rpc.FindValueRequest(kademlia.GetInstance().Me, valueID, target)
-			if err == nil {
-				result = append(result, val)
+			val := rpc.StoreRequest(kademlia.GetInstance().Me, target, value)
+			if !val {
+				completed = val
 			}
 		}()
 	}
 	wg.Wait()
-	for _, v := range result {
-		if v.ID == valueID {
-			return v, nil
-		}
+	if !completed {
+		return NodeStore(value)
+	} else {
+		return true
 	}
-	return stored.Value{}, errors.New("value not found")
 }
