754860965510ba733d319e0e15fc4e8f9d01b095
erik99s <erisre-8@student.ltu.se>
2022-09-28 16:23:34 +0200

    100% coverage on testBucket

--- src/node/bucket/bucket_test.go
+++ src/node/bucket/bucket_test.go
@@ -21,6 +21,72 @@ func TestNewBucket(t *testing.T) {
 	}
 }
 
+// Adds a single Contact
+func TestAddContact(t *testing.T) {
+	bucket := newBucket()
+	contact := GenreateAContact()
+	bucket.AddContact(contact)
+
+	if bucket.Len() == 0 {
+		t.FailNow()
+	}
+}
+
+// Tries to add a new contact to an already full bucket which will return bool=false
+func TestAddContactBucketFullNewContactNotAdded(t *testing.T) {
+	bucket := newBucket()
+	for i := 0; i < 20; i++ {
+		contact := GenreateAContact()
+		bucket.AddContact(contact)
+	}
+
+	contact := GenreateAContact()
+
+	_, isAdded := bucket.AddContact(contact)
+
+	if isAdded == true {
+		t.FailNow()
+	}
+
+}
+
+// Test to add the same contact with the same id twice which will give length == 1 and isAdded == false
+func TestAddSameContactTwice(t *testing.T) {
+	bucket := newBucket()
+	contact := GenreateAContact()
+	bucket.AddContact(contact)
+	_, isAdded := bucket.AddContact(contact)
+
+	if bucket.Len() != 1 && isAdded == true {
+		t.FailNow()
+	}
+}
+
+func TestRemoveContact(t *testing.T) {
+	bucket := newBucket()
+	contact := GenreateAContact()
+	bucket.AddContact(contact)
+	bucket.RemoveContact(contact)
+	if bucket.Len() != 0 {
+		t.FailNow()
+	}
+}
+
+// List of contacts with calculated lists is equal to the length of the bucket
+func TestCalcDistance(t *testing.T) {
+	bucket := newBucket()
+	contact1 := GenreateAContact()
+	contact2 := GenreateAContact()
+	bucket.AddContact(contact1)
+	bucket.AddContact(contact2)
+	contacts := bucket.GetContactAndCalcDistance(contact1.ID)
+
+	if len(contacts) != bucket.Len() {
+		t.FailNow()
+	}
+
+}
+
 func TestBucketLen(t *testing.T) {
 	bucket := newBucket()
 	len := bucket.Len()
