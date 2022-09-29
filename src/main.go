2bedabc067e3d70fef9f181b92a281a1e3232b2d
Denne213 <dennist1@live.se>
2022-09-29 12:00:49 +0200

    added test in main

--- src/main.go
+++ src/main.go
@@ -1,56 +1,30 @@
 package main
 
 import (
+	"D7024E/cli"
 	"D7024E/log"
 	"D7024E/network/server"
+	"D7024E/node/bucket"
+	"D7024E/node/contact"
 	"D7024E/node/id"
 	"D7024E/node/kademlia"
 	"D7024E/node/kademlia/algorithms"
-	"D7024E/node/stored"
-	"math/rand"
 	"net"
 	"time"
 )
 
 func main() {
-	// ip := net.IPv4(127, 0, 0, 1)
-	// port := 4001
-	// go server.UDPListener(ip, port)
-	// sender.UDPSender(ip, port, "this is the message")
-
-	// rt := bucket.GetInstance()
-	// rt.SetMe(contact.Contact{ID: id.NewRandomKademliaID(), Address: "this is address"})
-	// log.INFO("%v", rt.GetMe())
-	// val := stored.GetInstance()
-	// i := *id.NewRandomKademliaID()
-	// val.Store([]stored.Value{{Data: "this is the data", ID: i}})
-	// res, err := val.FindValue(i)
-	// if err != nil {
-	// 	log.ERROR("%v", err)
-	// } else {
-	// 	log.INFO("%v", res)
-	// }
-
+	kademlia.GetInstance()
 	go server.UDPListener(net.ParseIP(kademlia.GetInstance().Me.Address), 4001)
-	min := 2
-	max := 6
-	time.Sleep(time.Duration(rand.Intn(max-min)+min) * time.Second)
-	valID := *id.NewKademliaID("THIS IS THE VALUE")
-	if kademlia.GetInstance().Me.Address == "172.21.0.2" {
-		stored.GetInstance().Store([]stored.Value{{Data: "THIS IS THE VALUE", ID: valID}})
-		for {
-		}
-	}
-	// res := rpc.Ping(kademlia.GetInstance().Me, contact.Contact{Address: "172.21.0.2"})
-	// log.INFO("RES: %v", res)
-
-	res2, err := algorithms.NodeValueLookup(valID)
-	if err != nil {
-		log.INFO("%v", err)
+	time.Sleep(5 * time.Second)
+	if contact.GetInstance().Address == "172.21.0.2" {
+		contact.GetInstance().ID = id.NewKademliaID("172.21.0.2")
+		time.Sleep(10 * time.Second)
+		log.INFO("%v", bucket.GetInstance().FindClosestContacts(id.NewRandomKademliaID(), 20))
 	} else {
-		log.INFO("RES node value %v", res2)
+		algorithms.AddContact(contact.Contact{ID: id.NewKademliaID("172.21.0.2"), Address: "172.21.0.2"})
+		result := algorithms.NodeLookup(*id.NewKademliaID("172.21.0.2"))
+		log.INFO("%v", result)
 	}
-	for {
-
-	}
+	cli.CliListener()
 }
