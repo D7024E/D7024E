f35aaba7969392607eea478d61437d5f8bdb4572
Dennis Trollsfjord <dennist1@live.se>
2022-10-03 15:58:24 +0200

    test fix

--- src/node/kademlia/algorithms/nodeLookup_test.go
+++ src/node/kademlia/algorithms/nodeLookup_test.go
@@ -185,7 +185,6 @@ func TestRemoveDeadNodesAllAlive(t *testing.T) {
 
 // Verify that dead nodes are deleted.
 func TestRemoveDeadNodesAllDead(t *testing.T) {
-	kademlia.GetInstance()
 	batch := []contact.Contact{
 		{ID: id.NewRandomKademliaID(), Address: "127.21.0.2"},
 		{ID: id.NewRandomKademliaID(), Address: "127.21.0.3"},
