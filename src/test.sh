06d7e37c1bf9fa1581f39227821c29e5ab18b53f
Dennis Trollsfjord <dennist1@live.se>
2022-10-03 14:32:01 +0200

    updated test.sh file to give all file coverage

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,7 @@
 go test -coverprofile cover.out =./... ./...
 echo "-------------------------------------------------------------"
+go tool cover -func cover.out 
+echo "-------------------------------------------------------------"
 git clean -fxd --dry-run 
 echo "-------------------------------------------------------------"
 read -p "Do you want to clean [y/n]? " answer
