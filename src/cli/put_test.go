df65e1bdd0c09e31ce40b25360edeffbed97693d
Anders Dahl <andersoskardahl@hotmail.com>
2022-10-05 09:44:07 +0200

    put success test

--- src/cli/put_test.go
+++ src/cli/put_test.go
@@ -5,9 +5,18 @@ import (
 	"testing"
 )
 
+// Verify that a hash is returned on success.
+func TestPutSuccess(t *testing.T) {
+	var input string = "This is a test where a hash should be returned."
+	var res string = Put(input, NodeStoreMockSuccess)
+	if res == "" {
+		t.FailNow()
+	}
+}
+
 // Verify correct fail behaviour on correct input.
 func TestPutValidFail(t *testing.T) {
-	var input string = "put asdjasdpaoisjmmnijdsfa0"
+	var input string = "asdjasdpaoisjmmnijdsfa0"
 	var res string = Put(input, NodeStoreMockFail)
 	if res != "" {
 		t.FailNow()
