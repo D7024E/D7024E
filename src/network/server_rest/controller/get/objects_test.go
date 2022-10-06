564853ca0c8345b15a6ba7f6f938e07da374e654
Denne213 <dennist1@live.se>
2022-10-06 11:55:15 +0200

    added get tests

--- src/network/server_rest/controller/get/objects_test.go
+++ src/network/server_rest/controller/get/objects_test.go
@@ -12,8 +12,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Test
-func TestObjects(t *testing.T) {
+// Test GET "/objects/{hash}" if correct status and response is given. Test request that will get status 200.
+func TestObjects200(t *testing.T) {
 	// Create value
 	value := stored.Value{Data: "this is data"}
 	value.ID = *id.NewKademliaID(value.Data)
@@ -51,6 +51,79 @@ func TestObjects(t *testing.T) {
 
 	// Check the response body is what we expect.
 	expected := string(jsonValue)
+	body := strings.ReplaceAll(rr.Body.String(), "\n", "")
+	if body != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			body, expected)
+	}
+}
+
+// Test GET "/objects/{hash}" if correct status and response is given. Test request that will get status 400.
+func TestObjects400(t *testing.T) {
+	valueID := id.NewRandomKademliaID().String() + "a"
+	// Create request
+	route := "/objects/" + valueID
+	req, err := http.NewRequest("GET", route, nil)
+	if err != nil {
+		t.FailNow()
+	}
+
+	// Set request parameters
+	vars := map[string]string{
+		"hash": valueID,
+	}
+	req = mux.SetURLVars(req, vars)
+
+	// Test the request
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Objects)
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusBadRequest)
+	}
+
+	// Check the response body is what we expect.
+	expected := ""
+	body := strings.ReplaceAll(rr.Body.String(), "\n", "")
+	if body != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			body, expected)
+	}
+}
+
+// Test GET "/objects/{hash}" if correct status and response is given. Test request that will get status 404.
+func TestObjects404(t *testing.T) {
+	valueID := id.NewRandomKademliaID().String()
+
+	// Create request
+	route := "/objects/" + valueID
+	req, err := http.NewRequest("GET", route, nil)
+	if err != nil {
+		t.FailNow()
+	}
+
+	// Set request parameters
+	vars := map[string]string{
+		"hash": valueID,
+	}
+	req = mux.SetURLVars(req, vars)
+
+	// Test the request
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Objects)
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusNotFound)
+	}
+
+	// Check the response body is what we expect.
+	expected := ""
 	body := strings.ReplaceAll(rr.Body.String(), "\n", "")
 	if body != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
--- src/network/server_rest/controller/post/objects_test.go
+++ src/network/server_rest/controller/post/objects_test.go
@@ -14,8 +14,8 @@ type message struct {
 	Data string `json:"data"`
 }
 
-// Test POST "/objects" if correct status and response is given. Test request that will get status 200.
-func TestObjectsSuccess(t *testing.T) {
+// Test POST "/objects" if correct status and response is given. Test request that will get status 201.
+func TestObjects201(t *testing.T) {
 	// Create request
 	jsonMessage, err := json.Marshal(message{Data: "THIS IS THE DATA"})
 	if err != nil {
@@ -47,7 +47,7 @@ func TestObjectsSuccess(t *testing.T) {
 }
 
 // Test POST "/objects" if correct status and response is given. Test request that will get status 400.
-func TestObjectsFail(t *testing.T) {
+func TestObjects400(t *testing.T) {
 	// Create request
 	jsonMessage, err := json.Marshal(message{})
 	if err != nil {
