548980d85f3293838d7dc7bc299c0cbea414e626
Denne213 <dennist1@live.se>
2022-10-06 09:58:58 +0200

    added test for post route

---
+++ src/network/server_rest/controller/post/objects_test.go
@@ -0,0 +1,46 @@
+package post
+
+import (
+	"D7024E/node/id"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type message struct {
+	Data string `json:"data"`
+}
+
+func TestObjects(t *testing.T) {
+	// Create request
+	jsonMessage, err := json.Marshal(message{Data: "THIS IS THE DATA"})
+	if err != nil {
+		t.FailNow()
+	}
+	req, err := http.NewRequest("POST", "/objects", bytes.NewBuffer(jsonMessage))
+	if err != nil {
+		t.FailNow()
+	}
+
+	// Test the request
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Objects)
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated)
+	}
+
+	// Check the response body is what we expect.
+	expected := `"/objects/` + id.NewKademliaID("THIS IS THE DATA").String() + `"`
+	body := strings.ReplaceAll(rr.Body.String(), "\n", "")
+	if body != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
