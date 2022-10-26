3d6487f010f040339a7c8d6fcfcc201bc55a397d
Dennis Trollsfjord <dennist1@live.se>
2022-10-26 10:44:57 +0200

    minor fix

--- src/node/node.go
+++ src/node/node.go
@@ -3,7 +3,6 @@ package node
 import (
 	"D7024E/log"
 	"D7024E/node/contact"
-	"D7024E/node/id"
 	"D7024E/node/kademlia/algorithms"
 	"fmt"
 	"math/rand"
@@ -18,7 +17,7 @@ func init() {
 func StartKademliaNode() {
 	log.INFO("[NODE] - CONNECTED - [KADEMLIA NETWORK]")
 	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-	kClosest := algorithms.NodeLookup(*id.NewRandomKademliaID())
+	kClosest := algorithms.NodeLookup(*contact.GetInstance().ID)
 	kAddress := []string{}
 	for _, c := range kClosest {
 		kAddress = append(kAddress, c.Address)
